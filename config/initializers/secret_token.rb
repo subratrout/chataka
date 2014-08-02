43afe8ecf068c688f0e6aee127c32d3ca8eec7f5
Subrat Rout <subratnrout@gmail.com>
2014-08-02 00:27:17 -0700

    First commit on Chataka

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chataka::Application.config.secret_key_base = '1aea5107cc24e95baf8bf00176319efbb6ca324127972137b395f86f84dfb9cc93121559ddb233f45dbb5fd0b278f0fdfb96a29b8e54fab8f02481de29f0abd7'
